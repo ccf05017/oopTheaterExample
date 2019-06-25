2129c5818b6132813e18a5e6392d679898425615
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-06-25 09:29:53 +0900

    Ver3. - Second Encapsulation - Modify Audience

--- oopTheaterExample/src/com/jbj/theaterExample/Audience.java
+++ oopTheaterExample/src/com/jbj/theaterExample/Audience.java
@@ -8,7 +8,20 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+//    public Bag getBag() {
+//        return bag;
+//    }
+
+    public Long buy(Ticket ticket) {
+        if (this.bag.hasInvitation()) {
+            // 초대장이 있는 경우
+            this.bag.setTicket(ticket);
+            return 0L;
+        } else {
+            // 초대장이 없는 경우
+            this.bag.setTicket(ticket);
+            this.bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
--- oopTheaterExample/src/com/jbj/theaterExample/TicketSeller.java
+++ oopTheaterExample/src/com/jbj/theaterExample/TicketSeller.java
@@ -14,18 +14,7 @@ public class TicketSeller {
 //    }
 
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            // 초대장이 있는 경우
-            // 초대장이 있는데 왜 티켓 판매원한테 티켓을 삼?
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            // 초대장이 없는 경우
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 
 }
